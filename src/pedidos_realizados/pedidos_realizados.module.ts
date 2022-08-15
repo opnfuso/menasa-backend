4f3a7dfcc8f7c46039cc0241d768601b16eb3e3e
Mario Quevedo <opnfuso@gmail.com>
2022-08-14 20:56:06 -0500

    Add pedidos to controller

--- src/pedidos_realizados/pedidos_realizados.module.ts
+++ src/pedidos_realizados/pedidos_realizados.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
 import { PedidosRealizadosService } from './pedidos_realizados.service';
 import { PedidosRealizadosController } from './pedidos_realizados.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import {
+  PedidosRealizado,
+  PedidosRealizadoShcema,
+} from './schema/pedidos_realizado.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: PedidosRealizado.name, schema: PedidosRealizadoShcema },
+    ]),
+  ],
   controllers: [PedidosRealizadosController],
   providers: [PedidosRealizadosService],
 })
--- src/pedidos_standby/pedidos_standby.module.ts
+++ src/pedidos_standby/pedidos_standby.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
 import { PedidosStandbyService } from './pedidos_standby.service';
 import { PedidosStandbyController } from './pedidos_standby.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import {
+  PedidosStandby,
+  PedidosStandbySchema,
+} from './schema/pedidos_standby.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: PedidosStandby.name, schema: PedidosStandbySchema },
+    ]),
+  ],
   controllers: [PedidosStandbyController],
   providers: [PedidosStandbyService],
 })
