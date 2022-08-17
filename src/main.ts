6b4adf7f9513129b9ab8e68971dac149ebd08370
Mario Quevedo <opnfuso@gmail.com>
2022-08-17 11:31:54 -0500

    Enable CORS

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,7 @@ ConfigModule.forRoot();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
